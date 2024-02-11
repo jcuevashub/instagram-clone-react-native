b1a78bf89c6ad8f09366ad592fee7fd5070d3f2a
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-02-10 22:05:25 -0400

    Story components

---
+++ data/user.js
@@ -0,0 +1,42 @@
+export const USERS = [
+    {
+        "user": "ielliott",
+        "image": "https://cdnstorage.sendbig.com/unreal/female.webp"
+    },
+    {
+        "user": "williamslawrence",
+        "image": "https://i.pravatar.cc/300"
+    },
+    {
+        "user": "paul35",
+        "image": "https://i.pravatar.cc/300"
+    },
+    {
+        "user": "angela21",
+        "image": "https://i.pravatar.cc/300"
+    },
+    {
+        "user": "jgriffin",
+        "image": "https://i.pravatar.cc/300"
+    },
+    {
+        "user": "kathy32",
+        "image": "https://i.pravatar.cc/300"
+    },
+    {
+        "user": "mmoreno",
+        "image": "https://i.pravatar.cc/300"
+    },
+    {
+        "user": "mlawrence",
+        "image": "https://i.pravatar.cc/300"
+    },
+    {
+        "user": "donald63",
+        "image": "https://i.pravatar.cc/300"
+    },
+    {
+        "user": "anthonyburton",
+        "image": "https://i.pravatar.cc/300"
+    }
+]
