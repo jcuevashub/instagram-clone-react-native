5cf23fa504afbbce6cef99da250d825b247e2989
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-02-10 21:30:14 -0400

    Header component

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+
+export default function App() {
+  return (
+    <HomeScreen />
+  );
+}
---
+++ components/home/Header.jsx
@@ -0,0 +1,66 @@
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <Image style={styles.logo} source={require('../../assets/Instagram_logo.svg.png')} />
+            </TouchableOpacity>
+            <View style={styles.iconContiner}>
+                <TouchableOpacity>
+                    <Image source={require('../../assets/heart_icon.png')} style={styles.icon} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={styles.unreadBadge}>
+                        <Text style={styles.unreadBadgeText}>11</Text>
+                    </View>
+                    <Image source={require('../../assets/facebook_messenger_icon.png')} style={styles.icon} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginHorizontal: 20
+    },
+    iconContiner: {
+        flexDirection: 'row'
+    },
+    logo: {
+        width: 100,
+        height: 50,
+        resizeMode: 'contain',
+        tintColor: 'white'
+    },
+    icon: {
+        width: 30,
+        height: 30,
+        marginLeft: 10,
+        resizeMode: 'contain',
+        tintColor: 'white',
+    },
+    unreadBadge: {
+        backgroundColor: 'red',
+        position: 'absolute',
+        left: 20,
+        bottom: 18,
+        width: 25,
+        height: 18,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 100
+    },
+    unreadBadgeText: {
+        color: 'white',
+        fontWeight: '600'
+    }
+});
+
+export default Header
