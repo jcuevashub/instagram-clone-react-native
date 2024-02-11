83bd8435608beb3e6c69e37587de3f00546fb1d8
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-02-10 23:08:36 -0400

    Post Image done

--- components/home/Post.jsx
+++ components/home/Post.jsx
@@ -7,6 +7,7 @@ const Post = ({ post }) => {
         <View style={{ marginBottom: 30 }}>
             <Divider width={1} orientation='vertical' />
             <PostHeader post={post} />
+            <PostImage post={post} />
         </View>
     )
 }
@@ -23,6 +24,15 @@ const PostHeader = ({ post }) => (
             <Text style={{ color: 'white', marginLeft: 5, fontWeight: '700' }}>{post.user}</Text>
         </View>
         <Text style={{ color: 'white', fontWeight: '900' }}>...</Text>
+    </View>
+)
+
+const PostImage = ({ post }) => (
+    <View style={{ width: '100%', height: 450 }}>
+        <Image
+            source={{ uri: post.imageUrl }}
+            style={{ height: '100%', resizeMode: 'cover' }}
+        />
     </View>
 )
 
