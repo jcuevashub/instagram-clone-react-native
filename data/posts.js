1fae3f811cc737a992ed1e1138b15dfdc00374c4
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-02-10 22:54:15 -0400

    Post header done

---
+++ data/posts.js
@@ -0,0 +1,134 @@
+import { USERS } from "./user";
+
+export const POSTS = [
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "michelle91",
+        "likes": 567,
+        "caption": "Read these participant capital trouble special college.",
+        "profile_picture":"https://i.pravatar.cc/300",
+        "comments": [
+            {
+                "user": "amandaanderson",
+                "comments": "Million development should here home."
+            }
+        ]
+    },
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "lauraellis",
+        "likes": 2283,
+        "caption": "Mind sit vote first security finish space.",
+        "profile_picture":"https://i.pravatar.cc/300",
+        "comments": [
+            {
+                "user": "charles31",
+                "comments": "Matter reason law."
+            }
+        ]
+    },
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "amandaanderson",
+        "likes": 6195,
+        "caption": "Provide series view new direction house decision.",
+        "profile_picture": "https://fakeimg.pl/250x250/?text=andersonholly&font=lobster",
+        "comments": [
+            {
+                "user": "kristi11",
+                "comments": "Well cultural lot parent away."
+            }
+        ]
+    },
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "kristi11",
+        "likes": 1381,
+        "caption": "Perform exist bar development charge charge.",
+        "profile_picture":"https://i.pravatar.cc/300",
+        "comments": [
+            {
+                "user": "charles31",
+                "comments": "Radio debate significant teach to memory last."
+            }
+        ]
+    },
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "richard74",
+        "likes": 8014,
+        "caption": "But dog cover region crime past current.",
+        "profile_picture":"https://i.pravatar.cc/300",
+        "comments": [
+            {
+                "user": "ostein",
+                "comments": "Resource occur town politics."
+            }
+        ]
+    },
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "ostein",
+        "likes": 6348,
+        "caption": "Realize of strategy hospital deep have.",
+        "profile_picture":"https://i.pravatar.cc/300",
+        "comments": [
+            {
+                "user": "lauraellis",
+                "comments": "Your scientist which involve free."
+            }
+        ]
+    },
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "barbarabradford",
+        "likes": 8008,
+        "caption": "Your call specific power.",
+        "profile_picture":"https://i.pravatar.cc/300",
+        "comments": [
+            {
+                "user": "michelle91",
+                "comments": "Another position strong."
+            }
+        ]
+    },
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "ucox",
+        "likes": 8576,
+        "caption": "Decade far back front like.",
+        "profile_picture":"https://i.pravatar.cc/300",
+        "comments": [
+            {
+                "user": "ucox",
+                "comments": "Open rock guy however practice."
+            }
+        ]
+    },
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "christopher06",
+        "likes": 1054,
+        "caption": "Of whose exactly few.",
+        "profile_picture":"https://i.pravatar.cc/300",
+        "comments": [
+            {
+                "user": "charles31",
+                "comments": "Place year nation second worker student."
+            }
+        ]
+    },
+    {
+        "imageUrl": "https://i.pravatar.cc/500",
+        "user": "charles31",
+        "likes": 1582,
+        "caption": "Experience inside brother imagine maybe think service myself.",
+        "profile_picture":"https://i.pravatar.cc/300",
+        "comments": [
+            {
+                "user": "michelle91",
+                "comments": "Quite worker father despite difficult shoulder oil."
+            }
+        ]
+    }
+]
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,14 +1,24 @@
-import { SafeAreaView, StyleSheet} from 'react-native'
+import { ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
 import Header from '../components/home/Header';
 import Stories from '../components/home/Stories';
+import Post from '../components/home/Post';
+import SafeAreaView, { SafeAreaProvider } from 'react-native-safe-area-view';
+import { POSTS } from '../data/posts';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <Stories />
-    </SafeAreaView>
+    <SafeAreaProvider >
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <Stories />
+        <ScrollView >
+          {POSTS.map((post, index) => (
+            <Post post={post} key={index} />
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
 
